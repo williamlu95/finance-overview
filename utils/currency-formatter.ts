8b774bbe7169c41fcfd666d43ce657013dca632d
William Lu <williamlu95@gmail.com>
2024-06-01 00:55:46 -0700

    Fix negative bug

--- utils/currency-formatter.ts
+++ utils/currency-formatter.ts
@@ -1,7 +1,4 @@
 export const formatToDollars = (amount: number): string =>
   new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
 
-export const formatFromDollars = (dollars: string): number =>
-  dollars.startsWith('-')
-    ? -Number(dollars.replace(/[^0-9.-]+/g, ''))
-    : Number(dollars.replace(/[^0-9.-]+/g, ''));
+export const formatFromDollars = (dollars: string): number => Number(dollars.replace(/[$,]+/g, ''));
