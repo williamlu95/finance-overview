8565fc007a2def41e4080d18e87f9d73f27e8e47
William Lu <52891447+williamlu95@users.noreply.github.com>
2024-06-01 17:07:01 -0700

    Update CreditCardProgress.tsx

--- components/CreditCardProgress.tsx
+++ components/CreditCardProgress.tsx
@@ -8,6 +8,7 @@ type Props = {
 
 export default function CreditCardProgress({ creditCard }: Props): JSX.Element { 
   const severity = creditCard.difference === "$0.00" ? '#395D42' : '#BF8211';
+// #AB161A for negative balance
 
   return (
     <>
