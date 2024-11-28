e4dd8252cbaf3f1690159c04f2d985dd2b2edf64
William Lu <williamlu95@gmail.com>
2024-11-28 10:45:35 -0800

    Adjust cards

---
+++ constants/credit-card.ts
@@ -0,0 +1,4 @@
+export const JOINT_CREDIT_CARD: Record<string, string> = Object.freeze({
+    AMEX_GOLD: 'AMEX Gold',
+    MARRIOTT_BOUNDLESS: 'Marriott Boundless'
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { CreditCardRowType, FinanceDataType } from "@/types/finance";
 import { Container, Grid, Stack } from "@mui/material";
 import CreditCardProgress from "@/components/CreditCardProgress";
 import MinimumBalance from "@/components/MinimumBalance";
+import { JOINT_CREDIT_CARD } from "@/constants/credit-card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,15 +14,26 @@ export const getServerSideProps = (async () => {
 });
 
 export default function Home({ data } : { data: FinanceDataType }) {
-  const cards: CreditCardRowType[] = [...data.creditCards, data.food];
+  const personalCards: CreditCardRowType[] = data.creditCards.filter(cc => !Object.values(JOINT_CREDIT_CARD).includes(cc.name));
+  const jointCards: CreditCardRowType[] = [...data.creditCards.filter(cc => Object.values(JOINT_CREDIT_CARD).includes(cc.name)), data.food];
 
   return (
     <Container>
       <Stack height="100vh" justifyContent="center" spacing={3}>
         <Grid container rowGap={1}>
-            <Grid xs={12} item ml={0.5}>Balances</Grid>
+            <Grid xs={12} item ml={0.5}>Personal</Grid>
 
-            {cards.map((cc) => (
+            {personalCards.map((cc) => (
+              <Grid item xs={6} key={cc.name}>
+                <CreditCardProgress creditCard={cc} />
+              </Grid>
+            ))}
+        </Grid>
+
+        <Grid container rowGap={1}>
+            <Grid xs={12} item ml={0.5}>Joint</Grid>
+
+            {jointCards.map((cc) => (
               <Grid item xs={4} key={cc.name}>
                 <CreditCardProgress creditCard={cc} />
               </Grid>
@@ -35,11 +47,11 @@ export default function Home({ data } : { data: FinanceDataType }) {
             <MinimumBalance name="Joint" balances={data.joint} />
           </Grid>
 
-          <Grid item xs={12} alignItems="center">
+          <Grid item xs={6} alignItems="center">
             <MinimumBalance name="Personal" balances={data.personal} />
           </Grid>
 
-          <Grid item xs={12} alignItems="center">
+          <Grid item xs={6} alignItems="center">
             <MinimumBalance name="Mother" balances={data.mother} />
           </Grid>
           
